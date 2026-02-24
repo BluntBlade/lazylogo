c41f84d992c0fdd08afe855b102fe1af2aafb93c
Liang Tao <amethyst.black@gmail.com>
2026-02-24 23:46:26 +0800

    ADD: Add the implementation of the class Lexer and Token.

---
+++ src/interpreter.js
@@ -0,0 +1,83 @@
+class Token {
+    static NO_MORE = "NO_MORE";
+    static NOT_FOUND = "NOT_FOUND";
+    static WORD = "WORD";
+    static NUMBER = "NUMBER";
+    static VARIABLE = "VARIABLE";
+    static LIST_OPENING = "LIST_OPENING";
+    static LIST_CLOSING = "LIST_CLOSING";
+    static ARRAY_OPENING = "ARRAY_OPENING";
+    static ARRAY_CLOSING = "ARRAY_CLOSING";
+    static GROUP_OPENING = "GROUP_OPENING";
+    static GROUP_CLOSING = "GROUP_CLOSING";
+    static SIGN_PLUS = "SIGN_PLUS";
+    static SIGN_MINUS = "SIGN_MINUS";
+    static SIGN_STAR = "SIGN_STAR";
+    static SIGN_SLASH = "SIGN_SLASH";
+}
+
+class Lexer {
+    static HAS_NO_VALUE = function () { return null; };
+    static HAS_ONE_VALUE = function (m) { return m[1]; };
+
+    static PATTERNS = {
+        WORD:          {"value": Lexer.HAS_ONE_VALUE, "pattern": /^["']([_A-Za-z][_A-Za-z0-9]*)$/ },
+        NUMBER:        {"value": Lexer.HAS_ONE_VALUE, "pattern": /^([-+]?(?:[0-9]*\.[0-9]+|[0-9]+)(?:[eE][-+]?[0-9]+)?)$/ },
+        VARIABLE:      {"value": Lexer.HAS_ONE_VALUE, "pattern": /^:([_A-Za-z][_A-Za-z0-9]*$)/ },
+        LIST_OPENING:  {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\[$/ },
+        LIST_CLOSING:  {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\]$/ },
+        ARRAY_OPENING: {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\{$/ },
+        ARRAY_CLOSING: {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\}$/ },
+        GROUP_OPENING: {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\($/ },
+        GROUP_CLOSING: {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\)$/ },
+        SIGN_PLUS:     {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\+$/ },
+        SIGN_MINUS:    {"value": Lexer.HAS_NO_VALUE,  "pattern": /^-$/ },
+        SIGN_STAR:     {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\*$/ },
+        SIGN_SLASH:    {"value": Lexer.HAS_NO_VALUE,  "pattern": /^\/$/ },
+    };
+
+    constructor (s) {
+        this.components = s.split(/\s/);
+        this.index = 0;
+    };
+
+    next_token () {
+        if (this.index == this.components.length) {
+            return {
+                "matched": false,
+                "token": Token.NO_MORE,
+                "raw": "",
+            };
+        }
+
+        const text = this.components[this.index];
+        for (const key in Lexer.PATTERNS) {
+            const meta = Lexer.PATTERNS[key];
+
+            const m = text.match(meta["pattern"]);
+            if (! m) continue;
+
+            this.index += 1;
+            return {
+                "matched": true,
+                "token": key,
+                "raw": text,
+                "value": meta["value"](m),
+            };
+        } // for
+
+        return {
+            "matched": false,
+            "token": Token.NOT_FOUND,
+            "raw": text,
+        };
+    };
+
+    test () {
+        while (true) {
+            const tkn = this.next_token();
+            console.log(tkn);
+            if (! tkn.matched) break;
+        } // while
+    }
+}
